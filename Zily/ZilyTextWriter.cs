d1e144a1a9930f4f3fc9a7a991644edc76dd6222
SAPTeamDEV <111853937+SAPTeamDEV@users.noreply.github.com>
2023-04-21 09:55:49 +0330

    Add ZilyTextWriter

---
+++ Zily/ZilyTextWriter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SAPTeam.Zily
+{
+    /// <summary>
+    /// Represents a standard text writer for interacting with the <see cref="ZilyStream"/>.
+    /// </summary>
+    public class ZilyTextWriter : TextWriter
+    {
+        StringBuilder sb;
+        ZilyStream stream;
+
+        /// <inheritdoc/>
+        public override Encoding Encoding => Encoding.Unicode;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ZilyTextWriter"/>.
+        /// </summary>
+        /// <param name="zilyStream">
+        /// The underlying Zily stream.
+        /// </param>
+        public ZilyTextWriter(ZilyStream zilyStream)
+        {
+            stream = zilyStream;
+            sb = new StringBuilder();
+            NewLine = "\n";
+        }
+
+        /// <inheritdoc/>
+        public override void Write(char value)
+        {
+            sb.Append(value);
+        }
+
+        /// <inheritdoc/>
+        public override void Flush()
+        {
+            stream.Send(HeaderFlag.Write, sb.ToString());
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            sb.Clear();
+            sb = null;
+
+            stream = null;
+        }
+    }
+}
