c27d6e925f57266874b0b1429a9414e5664f5110
Aeliux <itsaeliux@gmail.com>
2023-07-09 20:16:25 +0330

    convert header flags to constants
    Signed-off-by: Aeliux <itsaeliux@gmail.com>

---
+++ src/ZilyHeaderFlag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAPTeam.Zily
+{
+    /// <summary>
+    /// Represents header flags for identifying zily packets.
+    /// </summary>
+    public class ZilyHeaderFlag
+    {
+        /// <summary>
+        /// An unknown flag.
+        /// </summary>
+        public const int Unknown = 0;
+
+        /// <summary>
+        /// Indicates a normal state
+        /// </summary>
+        public const int Ok = 1;
+
+        /// <summary>
+        /// Indicates an abnormal state. The error text must be sent with this header flag.
+        /// </summary>
+        public const int Fail = 2;
+
+        /// <summary>
+        /// Indicates a potential abnormal state.
+        /// </summary>
+        public const int Warn = 3;
+    }
+}
